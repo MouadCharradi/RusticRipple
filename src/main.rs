9ec1293b197aaac776b13b832deec52387deeffd
Mouad Charradi <charradimouad58@gmail.com>
2023-10-03 09:24:39 +0100

    main: Some refactoring and cleanups
    Signed-off-by: Mouad Charradi <charradimouad58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![allow(unused, dead_code)]
 use std::io::Write;
-use std::str::FromStr;
-use std::{net::TcpListener, io::Read};
 use std::path::PathBuf;
-
+use std::str::FromStr;
+use std::{io::Read, net::TcpListener};
 
 struct HTTPRequest {
     method: String,
@@ -11,24 +10,33 @@ struct HTTPRequest {
     version: String,
 }
 
+impl From<Vec<&str>> for HTTPRequest {
+    fn from(header: Vec<&str>) -> Self {
+        Self {
+            method: header[0].to_owned(),
+            path: PathBuf::from_str(header[1]).unwrap(),
+            version: header[2].to_owned(),
+        }
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
 
-    for stream in listener.incoming(){
-        let mut http_request;
+    for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         let mut buffer = vec![0u8; 8192];
         let read_amount = stream.read(&mut buffer).unwrap();
         let request = std::str::from_utf8(&buffer[..read_amount]).unwrap();
-        if let Some(first_line) = request.lines().next(){
-            let header:Vec<&str> = first_line.split(' ').collect();
-            if header.len() != 3{
+        if let Some(first_line) = request.lines().next() {
+            let header: Vec<&str> = first_line.split(' ').collect();
+            if header.len() != 3 {
                 continue;
             }
-            http_request = HTTPRequest{ method: header[0].to_owned(), path: PathBuf::from_str(header[1]).unwrap(), version: header[2].to_owned() };
-            let mut response = "HTTP/1.1 200 OK\r\n\r\n".to_owned();
-            response.push_str(include_str!("index.html"));
-            stream.write_all(response.as_bytes()).unwrap();
+            let http_request = HTTPRequest::from(header);
         }
+        let mut response = "HTTP/1.1 200 OK\r\n\r\n".to_owned();
+        response.push_str(include_str!("index.html"));
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
